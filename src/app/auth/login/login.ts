3e4a8e475c8bac26c0615ef22aa294d43bff7cae
ImEtienne <graddykibongani@gmail.com>
2026-02-19 12:18:13 +0100

    add form login

--- src/app/auth/login/login.ts
+++ src/app/auth/login/login.ts
@@ -1,11 +1,25 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { InputTextModule } from 'primeng/inputtext';
+import { ButtonModule } from 'primeng/button';
+import { FloatLabelModule } from 'primeng/floatlabel';
+import { InputGroupModule } from 'primeng/inputgroup';
+import { InputGroupAddonModule } from 'primeng/inputgroupaddon';
+
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [FormsModule, InputTextModule, ButtonModule, FloatLabelModule, InputGroupModule, InputGroupAddonModule],
   templateUrl: './login.html',
   styleUrl: './login.scss',
+  standalone:true,
 })
 export class Login {
+  email = '';
+  password = '';
 
+
+   login() {
+    console.log(this.email, this.password);
+  }
 }
