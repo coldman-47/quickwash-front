299817f9e712b1162af6bd70531ca6288ac31065
ImEtienne <graddykibongani@gmail.com>
2026-02-19 12:49:05 +0100

    feat: add register

--- src/app/auth/register/register.ts
+++ src/app/auth/register/register.ts
@@ -1,11 +1,31 @@
 import { Component } from '@angular/core';
+import { InputTextModule } from 'primeng/inputtext';
+import { ButtonModule } from 'primeng/button';
+import { InputGroupModule } from 'primeng/inputgroup';
+import { InputGroupAddonModule } from 'primeng/inputgroupaddon';
+import { FormsModule } from '@angular/forms';
+
 
 @Component({
   selector: 'app-register',
-  imports: [],
+  imports: [InputTextModule, ButtonModule, InputGroupModule, InputGroupAddonModule, FormsModule],
   templateUrl: './register.html',
   styleUrl: './register.scss',
 })
 export class Register {
+  firstName = '';
+  lastName = '';
+  email = '';
+  password = '';
 
+  register() {
+    const payload = {
+      firstName: this.firstName.trim(),
+      lastName: this.lastName.trim(),
+      email: this.email.trim().toLowerCase(),
+      password: this.password,
+    };
+
+    console.log(payload);
+  }
 }
