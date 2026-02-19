0b562dc3bc0d4e9af956559d746d4cd6f7d8d519
ImEtienne <graddykibongani@gmail.com>
2026-02-19 15:01:34 +0100

    feat: add interceptor

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -4,12 +4,15 @@ import { routes } from './app.routes';
 import { providePrimeNG } from 'primeng/config';
 import Aura from '@primeuix/themes/aura';
 import { MessageService } from 'primeng/api';
+import { provideHttpClient, withInterceptors } from '@angular/common/http';
+import { authInterceptor } from './core/interceptors/auth-interceptor';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideRouter(routes),
     MessageService,
+    provideHttpClient(withInterceptors([authInterceptor])),
     providePrimeNG({
             theme: {
                 preset: Aura,options:{
